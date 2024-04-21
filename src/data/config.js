24f46ca344a3d14067f8aff1a39e914ff78c0808
jenadiusnicholaus <ucode888@gmail.com>
2024-04-21 20:43:55 +0300

    set up the project template

---
+++ src/data/config.js
@@ -0,0 +1,19 @@
+export const firebaseSettings = {
+  apiKey: "AIzaSyBLuE_IppP6ZYnx8Cxu7U3hYcGZJGBS2MU",
+  authDomain: "travelmonkeyapp-18542.firebaseapp.com",
+  databaseURL: "https://travelmonkeyapp-18542-default-rtdb.firebaseio.com/",
+  projectId: "travelmonkeyapp-18542",
+  storageBucket: "travelmonkeyapp-18542.appspot.com",
+  messagingSenderId: "935307301261",
+  appId: "1:935307301261:web:b7def30ec89267a1506dc4",
+};
+
+// export const firebaseSettings = {
+//   apiKey: "AIzaSyBLuE_IppP6ZYnx8Cxu7U3hYcGZJGBS2MU",
+//   authDomain: "travelmonkeyapp-18542.firebaseapp.com",
+//   projectId: "travelmonkeyapp-18542",
+//   storageBucket: "travelmonkeyapp-18542.appspot.com",
+//   messagingSenderId: "935307301261",
+//   appId: "1:935307301261:web:b7def30ec89267a1506dc4",
+//   measurementId: "G-QWPNJZSCGQ",
+// };
